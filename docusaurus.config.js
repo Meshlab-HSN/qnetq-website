4cd6bba57ce545f37096ab89f0ac2538ba86b10e
Thomas Huehn <thomas.huehn@hs-nordhausen.de>
2024-03-11 23:56:38 +0100

    test menu label changes
    Signed-off-by: Thomas Huehn <thomas.huehn@hs-nordhausen.de>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Tutorial_test2',
           },
           /* {to: '/blog', label: 'Blog', position: 'left'},
           {
