885f7c2c2d9d7fda152a1ccc68e968e9910e90ca
marc-b2 <marc_b2@web.de>
2024-06-13 13:00:17 +0200

    Implement correct hovering and onClick behaviour in map.

--- src/components/EuropeMap/MapChart.jsx
+++ src/components/EuropeMap/MapChart.jsx
@@ -1,63 +1,70 @@
-import React from "react"
-import { ComposableMap, Geographies, Geography } from "react-simple-maps"
-import mapdata from "./mapdata"
+import React from "react";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import mapdata from "./mapdata";
+import { useHistory } from '@docusaurus/router';
 
 const Map = () => {
+  const history = useHistory();
+
+  const countryConfig = ({ NEIGHBOR, PROJECT }) => {
+    let config = {
+      color: '',
+      project: PROJECT,
+      style: {},
+      onClick: null,
+    };
+
+    switch (NEIGHBOR) {
+      case 0:
+        config.color = '#FF8A8A';
+        config.style = { hover: { opacity: 0.8 } };
+        config.onClick = () => history.push(`/docs/testbeds/trusted_node`);
+        break;
+      case 1:
+        config.color = 'darkgrey';
+        config.style = { hover: { opacity: 0.8 } };
+        break;
+      case 2:
+        config.color = 'lightgrey';
+        config.style = { hover: { opacity: 0.8 } };
+        break;
+      default:
+        config.color = '#EAEAEA';
+    }
+
+    return config;
+  };
+
   return (
     <ComposableMap
-      // style={{ backgroundColor: 'pink' }}
       projection='geoMercator'
       projectionConfig={{
         scale: 77,
         center: [6, 57],
-        // rotate: [-15.0, -50.0, -3],
       }}
       stroke='white'
-      stroke-width={0.1}
+      strokeWidth={0.1}
       width='100'
       height='100'
     >
       <Geographies geography={mapdata.data}>
-        {(geographies) => {
-          return geographies.geographies.map((geo) => {
-            return <Geography
-              key={geo.rsmKey}
-              geography={geo}
-              fill={
-                countryColor(geo.properties.NEIGHBOR)
-              }
-              style={{
-                hover: {
-                  opacity: 0.8,
-                }
-              }
-            }
-            />;
-          });
-        }}
+        {(geographies) =>
+          geographies.geographies.map((geo) => {
+            const config = countryConfig(geo.properties);
+            return (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                fill={config.color}
+                style={config.style}
+                onClick={config.onClick}
+              />
+            );
+          })
+        }
       </Geographies>
     </ComposableMap>
   );
-};
-
-function countryColor(neighbor) {
-  var color;
-  
-  switch (neighbor) {
-    case 0:
-      color = '#FF8A8A';
-      break;
-    case 1:
-      color = 'darkgrey';
-      break;
-    case 2:
-      color = 'lightgrey';
-      break;
-    default:
-      color = '#EAEAEA';
-  }
-  
-  return color;
 };
 
 export default Map;
