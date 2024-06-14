81003b7d02d81be4ee038b8f59cbcbe9347703ed
marc-b2 <marc_b2@web.de>
2024-06-14 09:45:43 +0200

    Show EuroQCI project names when hovering over member country.

--- src/components/EuropeMap/MapChart.jsx
+++ src/components/EuropeMap/MapChart.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import mapdata from "./mapdata";
 import { useHistory } from '@docusaurus/router';
+import { Tooltip } from 'react-tooltip';
 
 const Map = () => {
   const history = useHistory();
@@ -36,34 +37,40 @@ const Map = () => {
   };
 
   return (
-    <ComposableMap
-      projection='geoMercator'
-      projectionConfig={{
-        scale: 77,
-        center: [6, 57],
-      }}
-      stroke='white'
-      strokeWidth={0.1}
-      width='100'
-      height='100'
-    >
-      <Geographies geography={mapdata.data}>
-        {(geographies) =>
-          geographies.geographies.map((geo) => {
-            const config = countryConfig(geo.properties);
-            return (
-              <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                fill={config.color}
-                style={config.style}
-                onClick={config.onClick}
-              />
-            );
-          })
-        }
-      </Geographies>
-    </ComposableMap>
+    <>
+      <ComposableMap
+        projection='geoMercator'
+        projectionConfig={{
+          scale: 77,
+          center: [6, 57],
+        }}
+        stroke='white'
+        strokeWidth={0.1}
+        width='100'
+        height='100'
+      >
+        <Geographies geography={mapdata.data}>
+          {(geographies) =>
+            geographies.geographies.map((geo) => {
+              const config = countryConfig(geo.properties);
+              return (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill={config.color}
+                  style={config.style}
+                  onClick={config.onClick}
+                  data-tooltip-id="do-not-remove-this"
+                  data-tooltip-content={config.project}
+                  data-tooltip-float="true"
+                />
+              );
+            })
+          }
+        </Geographies>
+      </ComposableMap>
+      <Tooltip id="do-not-remove-this" />
+    </>
   );
 };
 
